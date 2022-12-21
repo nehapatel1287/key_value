5b446441d3c55106723f9b07b68900c0058ba5c8
Neha Patel <nehap.1287@gmail.com>
2022-12-21 11:51:08 +0530

    fatched value with code of letter

--- src/components/KeyCode.js
+++ src/components/KeyCode.js
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from 'react'
 
 const KeyCode = () => {
     const [key,setKey]=useState("")
+    const[capture,setCapture]=useState("")
 
     const keypressed=(event)=>{
         setKey(event.keyCode)
+        
     }
 
     useEffect(()=>{
-        
+        document.addEventListener('keydown',letter,true)
     },[])
+
+    const letter=(e)=>{
+        setCapture(e.key)
+    }
+
   return (
     <div style={{marginTop:'2%'}}>
-    <input type="text" onKeyDown={(e) => keypressed(e)} />
-    <p>Key code is: {key}</p>
+    <input type="text" onKeyDownCapture={(e) => keypressed(e)} value={capture}/>
+    <p>Key Pressed {capture} ,Key code is: {key}</p>
     </div>
     
     
